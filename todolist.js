f3ca21f536dfe5fe2c9f9bddef6096256ffb37fc
ErnestoCapambo <ernestosikilitacapambo@gmail.com>
2024-02-22 11:53:30 +0100

    mdandoo todolist

--- todolist.js
+++ todolist.js
@@ -1,14 +1,63 @@
-//  Meu codigo js 
-const conteudo=document.createElement("table"),form=document.querySelector(".formulario");form.addEventListener("submit",function(a){a.preventDefault();const b=document.querySelector("#nome").value;if(localStorage.listuser){let a=JSON.parse(localStorage.listuser);if(0<a.length){const c=a[a.length-1],d=c.id+1,e=a.concat({id:d,nome:b});localStorage.setItem("listuser",JSON.stringify(e))}else{const a=JSON.parse(localStorage.listuser),c=a.concat({id:1,nome:b});localStorage.setItem("listuser",JSON.stringify(c))}}else localStorage.setItem("listuser",JSON.stringify([{id:1,nome:b}]));mostrarDados()});function mostrarDados(){const a=localStorage.listuser;if(a){const b=JSON.parse(a),c=document.querySelector("body");conteudo.className="tabela",conteudo.innerHTML="",conteudo.innerHTML=`
+const conteudo = document.createElement("table");
+const form = document.querySelector(".formulario");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const nome = document.querySelector("#nome").value;
+
+    if (localStorage.listuser) {
+        let userList = JSON.parse(localStorage.listuser);
+        if (userList.length > 0) {
+            const lastUser = userList[userList.length - 1];
+            const id = lastUser.id + 1;
+            userList = userList.concat({ id: id, nome: nome });
+            localStorage.setItem("listuser", JSON.stringify(userList));
+        } else {
+            userList = userList.concat({ id: 1, nome: nome });
+            localStorage.setItem("listuser", JSON.stringify(userList));
+        }
+    } else {
+        localStorage.setItem("listuser", JSON.stringify([{ id: 1, nome: nome }]));
+    }
+
+    mostrarDados();
+});
+
+function mostrarDados() {
+    const userListString = localStorage.listuser;
+    if (userListString) {
+        const userList = JSON.parse(userListString);
+        const body = document.querySelector("body");
+        conteudo.className = "tabela";
+        conteudo.innerHTML = "";
+        conteudo.innerHTML = `
             <tr>
                 <th>Id</th>
                 <th>Nome</th>
                 <th>Botao</th>
             </tr>
-        `,c.appendChild(conteudo);for(let a=0;a<b.length;a++){const c=b[a],d=`
+        `;
+        body.appendChild(conteudo);
+
+        for (let i = 0; i < userList.length; i++) {
+            const user = userList[i];
+            const row = `
                 <tr>
-                    <td>${c.id}</td>
-                    <td>${c.nome}</td>
-                    <td><button type="submit" onclick="eliminarUsuario(${c.id})">Eliminar</button></td>
+                    <td>${user.id}</td>
+                    <td>${user.nome}</td>
+                    <td><button type="submit" onclick="eliminarUsuario(${user.id})">Eliminar</button></td>
                 </tr>
-            `;conteudo.innerHTML+=d}}}function eliminarUsuario(a){const b=JSON.parse(localStorage.listuser),c=b.filter(b=>b.id!==a);localStorage.listuser=JSON.stringify(c),mostrarDados()}window.onload=mostrarDados;
+            `;
+            conteudo.innerHTML += row;
+        }
+    }
+}
+
+function eliminarUsuario(id) {
+    const userList = JSON.parse(localStorage.listuser);
+    const updatedList = userList.filter(user => user.id !== id);
+    localStorage.listuser = JSON.stringify(updatedList);
+    mostrarDados();
+}
+
+window.onload = mostrarDados;
